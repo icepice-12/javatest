97f53a6c3efb49e6e98750eae508cc0bb8f433dd
alice <alice@192.168.1.14>
2024-09-27 15:47:06 +0200

    added method 1

--- src/main/java/it/generationitaly/app/controller/App.java
+++ src/main/java/it/generationitaly/app/controller/App.java
@@ -5,7 +5,14 @@ public class App {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World!");
+		metodo1();
 
+	}
+	
+	private static void metodo1() {
+		
+		System.out.println("Sono nel metodo1");
+		
 	}
 
 }
